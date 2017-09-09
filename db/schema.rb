c27d12dbf610cc5c89d78683cb77c06e49c1f528
Lawrence Pan <lawrencefeipan@gmail.com>
2017-09-09 19:09:40 -0400

    time_slots table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909190634) do
+ActiveRecord::Schema.define(version: 20170909230806) do
 
   create_table "events", force: :cascade do |t|
     t.string "summary"
     t.string "description"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "time_slots", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.integer "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
