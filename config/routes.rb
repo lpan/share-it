45727c5c099a233cf7d8a96e6dfc967198492b55
Lawrence Pan <lawrencefeipan@gmail.com>
2017-09-10 10:48:19 -0400

    gen routes + controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  namespace :api do
+    resources :users, only: %i[create show] do
+      resources :events, only: %i[create show]
+    end
+  end
 end
