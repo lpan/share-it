dae41bdb4b824efb6cbca7430de9bb01ebd34970
Lawrence Pan <lawrencefeipan@gmail.com>
2017-09-12 18:42:44 -0400

    iso8601 time format

--- app/serializers/time_slot_serializer.rb
+++ app/serializers/time_slot_serializer.rb
@@ -2,4 +2,12 @@ class TimeSlotSerializer < ActiveModel::Serializer
   attributes :id, :description, :location, :start_time, :end_time
 
   belongs_to :event
+
+  def start_time
+    object.start_time.to_s(:iso_8601)
+  end
+
+  def end_time
+    object.end_time.to_s(:iso_8601)
+  end
 end
